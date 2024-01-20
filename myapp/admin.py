6fe218037c0a36a7da3fb79aecfe6207534a495b
phuyalbish <phuyalbish@gmail.com>
2024-01-20 22:33:23 +0545

    models and homepage created

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client, Client_Product_Bridge, Product, Company, Sales
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Client_Product_Bridge)
+admin.site.register(Company)
+admin.site.register(Sales)
